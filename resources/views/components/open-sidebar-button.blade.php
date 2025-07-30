01a0095b53ebd89065d088e06f17bd355df7a5bf
hojinlee <infohojin@gmail.com>
2025-07-30 16:38:06 +0900

    sidebar hambuger button

--- resources/views/components/open-sidebar-button.blade.php
+++ resources/views/components/open-sidebar-button.blade.php
@@ -7,10 +7,10 @@
     기능:
     - 모바일에서 사이드바를 열기 위한 햄버거 메뉴 버튼
     - lg:hidden 클래스로 데스크톱에서는 숨김
-    - sidebar.js의 JavaScript와 연동되어 작동
+    - TailwindPlus의 command 시스템을 사용하여 동작
 --}}
 
-<button id="openSidebarBtn" type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden">
+<button type="button" command="show-modal" commandfor="mobileSidebar" class="-m-2.5 p-2.5 text-gray-700 lg:hidden">
     <span class="sr-only">Open sidebar</span>
     <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon"
         aria-hidden="true" class="size-6">
